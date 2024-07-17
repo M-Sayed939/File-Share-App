1006338f9f34484682b21ac7aea0483ad23bae73
M-Sayed939 <104098022+M-Sayed939@users.noreply.github.com>
2024-07-17 16:02:31 +0300

    bulid

---
+++ src/main/java/com/example/FileShare/FileShareAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.FileShare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileShareAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FileShareAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/File/Share/App/FileShareAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.File.Share.App;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileShareAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
